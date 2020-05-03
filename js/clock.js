409e5c6413d89905e9befcd78ee54301c9b9e4f4
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-05-03 12:27:02 +0500

    Handled logic for darkmode and sunmode

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+const dmn = document.getElementById('dmn');
+const hr = document.getElementById('hr');
+const mn = document.getElementById('mn');
+const sc = document.getElementById('sc');
+
+setInterval(() => {
+    const day = new Date();
+    const hh = day.getHours() * 30;
+    const mm = day.getMinutes() * 6;
+    const ss = day.getSeconds() * 6;
+    hr.style.transform = `rotate(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotate(${mm}deg)`;
+    sc.style.transform = `rotate(${ss}deg)`;
+});
--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
-const time = document.getElementById('time');
-const second = document.getElementById('second');
-const dmn = document.getElementById('dmn');
-const hr = document.getElementById('hr');
-const mn = document.getElementById('mn');
-const sc = document.getElementById('sc');
-
-setInterval(() => {
-    const day = new Date();
-    const hh = day.getHours() * 30;
-    const mm = day.getMinutes() * 6;
-    const ss = day.getSeconds() * 6;
-    hr.style.transform = `rotate(${hh+(mm/12)}deg)`;
-    mn.style.transform = `rotate(${mm}deg)`;
-    sc.style.transform = `rotate(${ss}deg)`;
-    time.textContent = moment(day).format("HH:mm");
-    second.textContent = moment(day).format("ss");
-});
+state = {
+    sunMode: !!localStorage.getItem('sunMode', 0),
+    clickEvend() {
+        this.sunMode = !this.sunMode;
+        if(this.sunMode) localStorage.setItem('sunMode', 1);
+        else localStorage.removeItem('sunMode');
+    },
+}
